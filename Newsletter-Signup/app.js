e5808cacb02ede88bd80f91158085bc71dd142de
Ardhendu <ardhendujj002@gmail.com>
2021-06-02 13:41:07 +0530

    connected to milchimps through api

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const app = express();
+const https = require("https")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -14,7 +15,30 @@ app.post("/", function(req,res){
   let firstname = req.body.f_name;
   let lastname = req.body.l_name;
   let email = req.body.email
-  res.send(`${firstname},${lastname},${email}`);
+
+  var data = {
+    members:[{
+      email_address :email,
+      status: "subscribed",
+      merge_fields:{
+        FNAME:firstname,
+        LNAME:lastname
+      }
+    }]
+  };
+  let jsonData = JSON.stringify(data);
+  const url = 'https://us6.api.mailchimp.com/3.0/lists/627b540a99' ;
+  const option ={
+    method: "POST",
+    auth :"ardhendu:652c13d979648ec152762987f4f52898-us6"
+  }
+  const request = https.request(url, option , function(response){
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
 
 });
 
@@ -22,3 +46,8 @@ app.post("/", function(req,res){
 app.listen("4040",function(){
   console.log("Server is runnning at port 4040");
 })
+
+
+//  API key ---652c13d979648ec152762987f4f52898-us6
+//
+// audience id -627b540a99
